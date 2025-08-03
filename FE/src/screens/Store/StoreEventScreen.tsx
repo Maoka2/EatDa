c1604dcd7f4ae93025bdb53759b086722e3e5bd0
DESKTOP-HA97GMI\SSAFY <maoka294@naver.com>
2025-08-03 21:35:16 +0900

    Add: 가게 이벤트 페이지 추가

--- FE/src/screens/Store/StoreEventScreen.tsx
+++ FE/src/screens/Store/StoreEventScreen.tsx
@@ -2,7 +2,7 @@
 
 // 탭스위치에서 값이 === event 인 경우에 불러오기
 
-import React , {useState} from "react";
+import React, { useState } from "react";
 
 import {
   View,
@@ -21,32 +21,45 @@ import {
   TouchableWithoutFeedback,
   Keyboard,
 } from "react-native";
-import {eventData} from "../../data/eventData"
-import GridComponent from "../../components/GridComponent";
+import { eventData } from "../../data/eventData";
+import GridComponent, { eventItem } from "../../components/GridComponent";
+import DetailEventScreen from "./DetailEventScreen";
 
-export default function StoreEventScreen(){
+export default function StoreEventScreen() {
+  // N*M 구조 만들기용
+  const [containerWidth, setContainerWidth] = useState(0);
 
-      const [containerWidth, setContainerWidth] = useState(0);
-
-
-    return (
+  // 전체보기 || 상세보기 구분용
+  const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
+  return (
+    // 상세보기
+    <View style={[{ flex: 1 }, {marginVertical:20}]}>
+      {selectedIndex !== null ? (
+        <DetailEventScreen
+          events={eventData}
+          initialIndex={selectedIndex}
+          onClose={() => setSelectedIndex(null)}
+        ></DetailEventScreen>
+      ) : (
+        // 전체보기
         <FlatList
-        data={eventData}
-        keyExtractor={(item) => item.id}
-        onLayout={(e) => setContainerWidth(e.nativeEvent.layout.width)}
-        numColumns={2}
-        renderItem={({item, index}) => (
-           <GridComponent
-           item={item}
-           size={containerWidth/2}
-           index={index}
-           totalLength={eventData.length}
-           onPress={() =>{
-            console.log("상세보기 출력할거임")
-           }}></GridComponent>
-
-        )}
+          data={eventData}
+          keyExtractor={(item) => item.id}
+          onLayout={(e) => setContainerWidth(e.nativeEvent.layout.width)}
+          numColumns={3}
+          renderItem={({ item, index }) => (
+            <GridComponent
+              item={item}
+              size={containerWidth / 3}
+              index={index}
+              totalLength={eventData.length}
+              onPress={() => {
+                setSelectedIndex(index);
+              }}
+            ></GridComponent>
+          )}
         ></FlatList>
-
-    )
+      )}
+    </View>
+  );
 }
