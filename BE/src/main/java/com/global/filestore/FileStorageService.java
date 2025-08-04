b3fc4a0b4af969036aeeb09e692e5232ad00e51a
김건학 <kkh10002@naver.com>
2025-08-04 15:27:36 +0900

    feat: 파일 저장소 서비스 인터페이스 추가

---
+++ BE/src/main/java/com/global/filestore/FileStorageService.java
@@ -0,0 +1,28 @@
+package com.global.filestore;
+
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * 파일 저장소 서비스 인터페이스
+ */
+public interface FileStorageService {
+    /**
+     * 이미지 파일을 저장소에 저장
+     *
+     * @param file         저장할 이미지 파일
+     * @param relativePath 상대 경로
+     * @param originalName 원본 파일명
+     * @return 저장된 파일의 경로
+     */
+    String storeImage(final MultipartFile file, final String relativePath, final String originalName);
+
+    /**
+     * 비디오 파일을 저장소에 저장
+     *
+     * @param file         저장할 비디오 파일
+     * @param relativePath 상대 경로
+     * @param originalName 원본 파일명
+     * @return 저장된 파일의 경로
+     */
+    String storeVideo(final MultipartFile file, final String relativePath, final String originalName);
+}
