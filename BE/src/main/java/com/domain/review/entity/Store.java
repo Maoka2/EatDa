c1d0cbe434f6b7f38b725221c12b48d211d58e85
Ukj0ng <xb253q@gmail.com>
2025-08-06 01:31:25 +0900

    refactor: test에 필요없는 컬럼 삭제

--- BE/src/main/java/com/domain/review/entity/Store.java
+++ BE/src/main/java/com/domain/review/entity/Store.java
@@ -1,45 +1,29 @@
 package com.domain.review.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
+@Setter
 @Entity
 @Table(name = "store")
 public class Store {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, length = 100)
     private String name;
-
-    @Column(nullable = false, length = 255)
-    private String address;
 
     @Column(nullable = false)
     private Double latitude;
 
     @Column(nullable = false)
     private Double longitude;
-
-    @Column(nullable = false)
-    private String licenseUrl;
-
-    @Column(length = 20)
-    private String status;
-
-    @Column(nullable = false)
-    private Long ownerId;
-
-    @Column(updatable = false)
-    private LocalDateTime createdAt;
-
-    private LocalDateTime updatedAt;
 
     // H3 인덱스: 해상도 7 ~ 10까지 별도 컬럼
     @Column(name = "h3_index_7")
