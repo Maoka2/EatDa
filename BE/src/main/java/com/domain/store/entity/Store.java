ed169c346787082bdb8de45f64343d0a0203b43e
김건학 <kkh10002@naver.com>
2025-08-06 23:22:11 +0900

    feat: Store 엔티티에 위치 정보 및 작성자 필드 추가

--- BE/src/main/java/com/domain/store/entity/Store.java
+++ BE/src/main/java/com/domain/store/entity/Store.java
@@ -1,11 +1,15 @@
 package com.domain.store.entity;
 
+import com.domain.user.entity.User;
 import com.global.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
@@ -31,9 +35,47 @@ public class Store extends BaseEntity {
     @Column(length = 255)
     private String address;
 
+    @NotNull
+    @Column
+    private Double latitude;
+
+    @NotNull
+    @Column
+    private Double longitude;
+
+    @NotNull
+    @Column(columnDefinition = "TEXT")
+    private String licenseUrl;
+
+    @Column
+    private Long h3Index7;
+
+    @Column
+    private Long h3Index8;
+
+    @Column
+    private Long h3Index9;
+
+    @Column
+    private Long h3Index10;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "maker_id", nullable = false)
+    private User maker;
+
     @Builder
-    public Store(final String name, final String address) {
+    public Store(final String name, final String address, final Double latitude, final Double longitude,
+                 final String licenseUrl, final User maker, final Long h3Index7, final Long h3Index8,
+                 final Long h3Index9, final Long h3Index10) {
         this.name = name;
         this.address = address;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.licenseUrl = licenseUrl;
+        this.maker = maker;
+        this.h3Index7 = h3Index7;
+        this.h3Index8 = h3Index8;
+        this.h3Index9 = h3Index9;
+        this.h3Index10 = h3Index10;
     }
 }
