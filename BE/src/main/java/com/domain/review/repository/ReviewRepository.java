d7bc531e155e10579f12704123aede691362817d
김건학 <kkh10002@naver.com>
2025-08-07 20:01:48 +0900

    docs: 리뷰 관련 Repository에 JavaDoc 추가

--- BE/src/main/java/com/domain/review/repository/ReviewRepository.java
+++ BE/src/main/java/com/domain/review/repository/ReviewRepository.java
@@ -4,8 +4,18 @@ import com.domain.review.entity.Review;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * 리뷰 데이터베이스 접근을 위한 Repository 인터페이스
+ */
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    /**
+     * 사용자 ID로 리뷰 목록 조회
+     */
     List<Review> findByUserId(Long userId);
 
+    /**
+     * 가게 ID로 리뷰 목록 조회
+     */
     List<Review> findByStoreId(Long storeId);
 }
