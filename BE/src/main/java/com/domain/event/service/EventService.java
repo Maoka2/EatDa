18dd36e06e5cc8ab88e450af8ce265ae85200c13
Ukj0ng <xb253q@gmail.com>
2025-08-10 13:08:56 +0900

    feat: EventService에 이벤트 조회 및 확정 메서드 추가
    - getEventAssetStatus: 에셋 상태 조회
    - finalizeEvent: 이벤트 최종 확정

--- BE/src/main/java/com/domain/event/service/EventService.java
+++ BE/src/main/java/com/domain/event/service/EventService.java
@@ -1,11 +1,16 @@
 package com.domain.event.service;
 
 import com.domain.event.dto.request.EventAssetCreateRequest;
+import com.domain.event.dto.request.EventFinalizeRequest;
 import com.domain.event.dto.response.EventAssetRequestResponse;
+import com.domain.event.dto.response.EventFinalizeResponse;
 import com.global.dto.request.AssetCallbackRequest;
+import com.global.dto.response.AssetResultResponse;
 
 public interface EventService {
 
     EventAssetRequestResponse requestEventAsset(final EventAssetCreateRequest request, final Long userId);
     void handleEventAssetCallback(final AssetCallbackRequest<?> request);
+    AssetResultResponse getEventAssetStatus(final Long assetId, final Long userId);
+    EventFinalizeResponse finalizeEvent(final EventFinalizeRequest request);
 }
