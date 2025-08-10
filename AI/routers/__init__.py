1cef38c45006205de8a3cb6f48435c89e13d0018
SangIn <tkddls0127@naver.com>
2025-08-11 02:09:23 +0900

    feat: test routers 추가

--- AI/routers/__init__.py
+++ AI/routers/__init__.py
@@ -1,9 +1,8 @@
 """
 Routers package
 """
-
-from .generate import router as generate_router
 from .ocr import router as ocr_router
 from .ocr_receipt import router as ocr_receipt_router
+from .stream_test import router as stream_test_router
 
-__all__ = ["generate_router", "ocr_router", "ocr_receipt_router"]
+__all__ = ["ocr_router", "ocr_receipt_router", "stream_test_router"]
