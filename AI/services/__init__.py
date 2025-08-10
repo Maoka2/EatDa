968881df6fabd832eeda8666e19113c1af3a17b1
SangIn <tkddls0127@naver.com>
2025-08-11 01:58:37 +0900

    feat: 이미지 생성 service 추가

--- AI/services/__init__.py
+++ AI/services/__init__.py
@@ -4,14 +4,14 @@ Services package
 
 from .luma_service import luma_service
 from .runway_service import runway_service
+from .image_service import image_service
 from .gpt_service import gpt_service
-from .callback_service import callback_service
-from .ocr_menuboard_service import ocr_menuboard_service
+from .menuboard_ocr_service import menuboard_ocr_service
 
 __all__ = [
     "luma_service",
     "runway_service",
     "gpt_service",
-    "callback_service",
-    "ocr_menuboard_service",
+    "menuboard_ocr_service",
+    "image_service",
 ]
