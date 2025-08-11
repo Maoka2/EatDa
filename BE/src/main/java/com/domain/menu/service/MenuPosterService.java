4ee7c56e2e1b36965b36c57ba4dedf323cb5014f
Ukjong <xb253q@gmail.com>
2025-08-11 21:12:25 +0900

    feat: 메뉴 포스터 최종 확정 기능 finalizeMenuPoster 인터페이스 추가
    - MenuPosterFinalizeRequest/Response DTO 기반의 포스터 확정 프로세스 정의
    - 생성된 포스터 자산의 완료 단계 처리를 위한 도메인 경계 설정
    - 메뉴 포스터 생성 → 상태 조회 → 최종 확정의 전체 라이프사이클 완성

--- BE/src/main/java/com/domain/menu/service/MenuPosterService.java
+++ BE/src/main/java/com/domain/menu/service/MenuPosterService.java
@@ -1,7 +1,9 @@
 package com.domain.menu.service;
 
 import com.domain.menu.dto.request.MenuPosterAssetCreateRequest;
+import com.domain.menu.dto.request.MenuPosterFinalizeRequest;
 import com.domain.menu.dto.response.MenuPosterAssetRequestResponse;
+import com.domain.menu.dto.response.MenuPosterFinalizeResponse;
 import com.global.dto.request.AssetCallbackRequest;
 import com.global.dto.response.AssetResultResponse;
 
@@ -10,4 +12,5 @@ public interface MenuPosterService {
     MenuPosterAssetRequestResponse requestMenuPosterAsset(final MenuPosterAssetCreateRequest request, final String eaterMail);
     void handleMenuPosterAssetCallback(final AssetCallbackRequest<?> request);
     AssetResultResponse getMenuPosterAssetStatus(final Long assetId, final String eaterMail);
+    MenuPosterFinalizeResponse finalizeMenuPoster(final MenuPosterFinalizeRequest request);
 }
