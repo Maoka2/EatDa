9f4c3d2cc6f6168c6e71e68516a6ee9f760f696b
Ukjong <xb253q@gmail.com>
2025-08-13 15:40:22 +0900

    refactor: ActiveStoreEventResponse 팩토리 메서드에 Store 파라미터 추가

--- BE/src/main/java/com/domain/event/dto/response/ActiveStoreEventResponse.java
+++ BE/src/main/java/com/domain/event/dto/response/ActiveStoreEventResponse.java
@@ -2,19 +2,22 @@ package com.domain.event.dto.response;
 
 import com.domain.event.entity.EventAsset;
 import com.domain.event.entity.Event;
+import com.domain.store.entity.Store;
 
 import java.time.LocalDate;
 
 public record ActiveStoreEventResponse(
         Long eventId,
+        String storeName,
         String title,
         LocalDate startAt,
         LocalDate endAt,
         String postUrl
 ) {
-    public static ActiveStoreEventResponse from(Event event, EventAsset asset)  {
+    public static ActiveStoreEventResponse from(Event event, EventAsset asset, Store store)  {
         return new ActiveStoreEventResponse(
                 event.getId(),
+                store.getName(),
                 event.getTitle(),
                 event.getStartDate(),
                 event.getEndDate(),
