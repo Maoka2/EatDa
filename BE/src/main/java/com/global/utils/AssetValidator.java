2cfb690772dbad495de4b01dd3c96c463d66e480
김건학 <kkh10002@naver.com>
2025-08-14 20:57:17 +0900

    fix: 이미지 크기 검증 로직 추가 및 오류 코드 수정

--- BE/src/main/java/com/global/utils/AssetValidator.java
+++ BE/src/main/java/com/global/utils/AssetValidator.java
@@ -1,25 +1,28 @@
 package com.global.utils;
 
+import static com.global.constants.ErrorCode.ASSET_NOT_FOUND;
+
 import com.global.constants.ErrorCode;
 import com.global.constants.Status;
 import com.global.dto.request.AssetCallbackRequest;
 import com.global.entity.BaseAssetEntity;
 import com.global.exception.ApiException;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.multipart.MultipartFile;
-
 import java.util.List;
 import java.util.Objects;
-
-import static com.global.constants.ErrorCode.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 public class AssetValidator {
     private static final long MAX_IMAGE_SIZE_MB = 10;
+    private static final long MIN_IMAGE_SIZE_MB = 10;
     private static final int IMAGE_BYTE = 1024;
 
     private static final String LOG_IMAGE_TOO_LARGE =
             "[AssetValidator] 이미지 크기 초과 - filename: {}, size: {}MB";
+
+    private static final String LOG_IMAGE_TOO_SMALL =
+            "[AssetValidator] 이미지 크기 부족 - filename: {}, size: {}MB";
 
     private static final String LOG_ASSET_NOT_FOUND =
             "[AssetValidator] 에셋이 존재하지 않음 - assetId: {}";
@@ -27,11 +30,15 @@ public class AssetValidator {
     private static final String LOG_ASSET_URL_REQUIRED_WHEN_SUCCESS =
             "[AssetValidator] 성공 상태지만 assetUrl이 비어 있음 - assetId: {}";
 
-    public static void validateImages(final List<MultipartFile> images, ErrorCode errorCode) throws ApiException {
+    public static void validateImages(final List<MultipartFile> images) throws ApiException {
         for (MultipartFile file : images) {
             if (file.getSize() > MAX_IMAGE_SIZE_MB * IMAGE_BYTE * IMAGE_BYTE) {
                 log.warn(LOG_IMAGE_TOO_LARGE, file.getOriginalFilename(), file.getSize() / (IMAGE_BYTE * IMAGE_BYTE));
-                throw new ApiException(errorCode, file.getOriginalFilename());
+                throw new ApiException(ErrorCode.IMAGE_TOO_LARGE, file.getOriginalFilename());
+            }
+            if (file.getSize() < MIN_IMAGE_SIZE_MB * IMAGE_BYTE * IMAGE_BYTE) {
+                log.warn(LOG_IMAGE_TOO_SMALL, file.getOriginalFilename(), file.getSize() / (IMAGE_BYTE * IMAGE_BYTE));
+                throw new ApiException(ErrorCode.IMAGE_TOO_SMALL, file.getOriginalFilename());
             }
         }
     }
